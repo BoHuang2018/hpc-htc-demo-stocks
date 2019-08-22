539aafdc2b9bce9d787e56aca6efb759aac2d776
BoHuang <tigerhuangbo@icloud.com>
2019-08-22 14:23:48 +0200

    Add some comments in script_generator_runner.py

--- script_generator_runner.py
+++ script_generator_runner.py
@@ -1,3 +1,17 @@
+"""
+This is the entrance program of the whole project.
+
+It implements such a process:
+    main() --> _parse_args() --> get_all_symbols_in_rows()
+        --> generate_condor_submit_trigger_text() --> generate_condor_sh_file()
+            --> trigger the whole project by subprocess.call()
+                --> watch the number of csv files in Cloud Storage's bucket
+                    --> report jobs are done
+
+The generated .txt and .sh files would be transported to submitter machine of the HTCondor cluster, and
+do their jobs there.
+"""
+
 import argparse
 import csv
 import subprocess
@@ -7,13 +21,21 @@ import pandas_datareader as web
 
 def generate_condor_submit_trigger_text(queue):
     """
-    To generate a text file looks like htcondor-submit-tasks
-    :param csv_file_queue: Temporally, it is the file nasdaq_symbols_all.csv
+    To generate a .txt-file to manage the submitter machine
+    :param queue: Int. Number of jobs the will be submitted to worker machines from submitter machine to worker machines.
+                  Now the number is decided by number of groups of stock symbols.
+                  For example, we divide the 8856 stock symbols to 128 groups, queue = 128.
     :return:
     """
     with open('htcondor-submit-tasks-app.txt', 'w') as htc:
         htc.write('executable              = task_randomwalk_process-app.sh \n')
         htc.write('arguments               = $(Process) \n')
+        #
+        # Look at the functions mentioned in following two lines
+        # randomwalk_multiprocess.py : this program will be runned in each worker machine
+        # nasdaq_symbols_in_rows.csv : this file stores all stock symbols and organize them into lines,
+        #                              each line has a certain number of symbols.
+        #
         htc.write('transfer_input_files    = randomwalk_multiprocess.py,nasdaq_symbols_in_rows.csv \n')
         htc.write('Transfer_Output_Files   = "" \n')
         htc.write('when_to_transfer_output = ON_EXIT \n')
@@ -26,7 +48,8 @@ def generate_condor_submit_trigger_text(queue):
 
 def generate_condor_sh_file(start_date, end_date):
     """
-    To generate a .sh file looks like task_random_process.sh
+    To generate a .sh file to manage worker machines in the following process:
+        get a row of symbols --> do simulation and generate csv file --> transport csv file to Cloud Storage
     :param start_date: Str. The start date of historical stock prices. For example '2017-01-01'
     :param end_date: Str. The end date of historical stock prices. For example '2019-01-01'
     :param stock_symbol: Str. Symbol of stock. Like AAPL
@@ -34,18 +57,17 @@ def generate_condor_sh_file(start_date, end_date):
     """
     with open('task_randomwalk_process-app.sh', 'w') as trp:
         trp.write('#! /bin/bash \n')
-        trp.write('index=$(($1+1)) \n')
-        trp.write('stockfile=nasdaq_symbols_in_rows.csv \n')
-        # trp.write('stock_symbols_sequence=$(awk "NR == ${index} {print; exit}" ${stockfile} | cut -d, -f1) \n')
-        trp.write('stock_symbols_sequence=$(awk "NR == ${index} {print; exit}" ${stockfile}) \n')
+        trp.write('index=$(($1+1)) \n')  # for-loop the rows of the csv file storing stock symbols
+        trp.write('stockfile=nasdaq_symbols_in_rows.csv \n')  # this csv stores all stock symbols
+        trp.write('stock_symbols_sequence=$(awk "NR == ${index} {print; exit}" ${stockfile}) \n')  # get a row
         trp.write('export HOME=`pwd` \n')
-        trp.write('chmod +x ./randomwalk_multiprocess.py \n')
+        trp.write('chmod +x ./randomwalk_multiprocess.py \n')   # trigger the main program to work
         trp.write('./randomwalk_multiprocess.py --start_date %s --end_date %s '
                   '--stock_symbols_list ${stock_symbols_sequence} \n'
                   % (start_date, end_date))
         trp.write('CLOUDSDK_PYTHON=/usr/bin/python '
                   'gsutil -m cp *.csv gs://hpc-htc-demo-stocks/stock_simulations_based_on_%s_%s/'
-                  % (start_date, end_date))
+                  % (start_date, end_date))  # transport result to Cloud Storage
     return True
 
 
@@ -60,6 +82,14 @@ def _parse_args():
 
 
 def get_all_symbols_in_rows(rows, output_csv_name='nasdaq_symbols_in_rows.csv'):
+    """
+    Scrape all stock symbols from Nasdaq Company List and store them in a csv file by rows.
+    :param rows: Int. Number of rows of stock symbols the csv file will have. Note that number of symbols in each row
+                      would be same except the last row.
+    :param output_csv_name: Int. Name of the csv file
+    :return: Int. Total number of stock symbols in the Nasdaq Company List.
+                  This returning would be used to judge the HTCondor cluster has done all jobs or not.
+    """
     all_nasdaq = web.get_nasdaq_symbols().index
     number_of_normal_rows = len(all_nasdaq) // (rows-1)
     number_of_last_row = len(all_nasdaq) % (rows-1)
